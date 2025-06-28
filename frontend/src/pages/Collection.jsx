bb963a4b8efd5b4af94479ddfab59b534d963a22
leowu0329 <ryowuandjanet@gmail.com>
2025-06-28 18:20:36 +0800

    add filter title in collection component

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,5 +1,27 @@
+import { useContext, useState } from 'react';
+import { ShopContext } from './../context/ShopContext';
+
 const Collection = () => {
-  return <div>Collection</div>;
+  const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
+
+  return (
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+      {/* Filter options */}
+      <div className="min-w-60">
+        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">
+          FILTERS
+        </p>
+
+        {/* Catagory Filter */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 mt-6 ${
+            showFilter ? '' : 'hidden'
+          } sm:block`}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default Collection;
