67ae10a4d060763bb06dc21b038afa47c4f26184
leowu0329 <ryowuandjanet@gmail.com>
2025-06-28 18:01:11 +0800

    created ourpolicy component + add another feature

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Hero from '../components/Hero';
 import LatestCollection from '../components/LatestCollection';
 import BestSeller from '../components/BestSeller';
+import OurPolicy from '../components/OurPolicy';
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Hero />
       <LatestCollection />
       <BestSeller />
+      <OurPolicy />
     </div>
   );
 };
