49486d5e643bdb5c770bffc0465074b57b707f65
leowu0329 <ryowuandjanet@gmail.com>
2025-06-28 18:41:25 +0800

    add title heading in collection component

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from 'react';
 import { ShopContext } from './../context/ShopContext';
 import { assets } from '../assets/assets';
+import Title from './../components/Title';
 
 const Collection = () => {
   const { products } = useContext(ShopContext);
@@ -72,6 +73,13 @@ const Collection = () => {
               Winterwear
             </p>
           </div>
+        </div>
+      </div>
+
+      {/* Right Side */}
+      <div className="flex-1">
+        <div className="flex justify-between text-base sm:text-2xl mb-4">
+          <Title text1={'All'} text2={'COLLECTIONS'} />
         </div>
       </div>
     </div>
