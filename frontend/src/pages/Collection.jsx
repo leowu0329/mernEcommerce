e225d44c11085dfd281d391ee773a4d4bacb5175
leowu0329 <ryowuandjanet@gmail.com>
2025-06-28 18:25:02 +0800

    add categories option in collection component

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -18,7 +18,26 @@ const Collection = () => {
           className={`border border-gray-300 pl-5 py-3 mt-6 ${
             showFilter ? '' : 'hidden'
           } sm:block`}
-        ></div>
+        >
+          <p>CATEGORIES</p>
+
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input className="w-4" type="checkbox" value={'Men'} />
+              Men
+            </p>
+
+            <p className="flex gap-2">
+              <input className="w-4" type="checkbox" value={'Women'} />
+              Women
+            </p>
+
+            <p className="flex gap-2">
+              <input className="w-4" type="checkbox" value={'kids'} />
+              Kids
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
