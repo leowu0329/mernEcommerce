36bf7cbbd317c9ba058130fe0dc14c00445dcc91
leowu0329 <ryowuandjanet@gmail.com>
2025-06-29 18:06:01 +0800

    add function for removing product and single product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -80,9 +80,26 @@ const listProducts = async (req, res) => {
 };
 
 // function for removing product
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+  try {
+    await productModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: 'Product Removed Successfully' });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // function for single product info
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const product = await productModel.findById(productId);
+    res.json({ success: true, product });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export { listProducts, addProduct, removeProduct, singleProduct };
